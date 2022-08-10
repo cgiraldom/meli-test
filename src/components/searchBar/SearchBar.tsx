4acadf632994165b4bdfa2707ca69afe3a4a7a6e
Carlos Eduardo GiraldoMontero <carloseduardogiraldomontero@gmail.com>
2022-08-10 10:19:08 -0500

    Add SearchBar styles

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Logo from '../../assets/Logo_ML_2x.png';
+import SearchIcon from '../../assets/ic_Search@2x.png';
 import './SearchBar.scss';
 
 export const SearchBar = (): JSX.Element => {
   return (
     <header className="search-bar-container">
       <img src={Logo} alt="logo" />
+      <input className="search-input" type="search" placeholder="Nunca dejes de buscar" />
+      <button className="search-button" name="search">
+        <img className="search-icon" src={SearchIcon} alt="search-icon" />
+      </button>
     </header>
   );
 };
