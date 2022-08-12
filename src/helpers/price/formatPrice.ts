378c90a0ad0cf6421374f88a89057375ad87e0ab
Carlos Eduardo GiraldoMontero <carloseduardogiraldomontero@gmail.com>
2022-08-12 16:48:47 -0500

    Refactor format number function
    to use browser locale

--- src/helpers/price/formatPrice.ts
+++ src/helpers/price/formatPrice.ts
@@ -3,6 +3,8 @@ import { ItemPrice } from '../../domain/item';
 export function formatPrice(price: ItemPrice): string {
   const { amount, decimals, currency } = price;
   const fullPrice = amount + decimals / (10 * amount.toString().length);
+  const userLocale =
+    navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
 
-  return new Intl.NumberFormat('en-IN', { style: 'currency', currency }).format(fullPrice);
+  return new Intl.NumberFormat(userLocale, { style: 'currency', currency }).format(fullPrice);
 }
