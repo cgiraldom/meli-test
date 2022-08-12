fc4a7e46ce9973dfb032fd22d571a93ea275b0f9
Carlos Eduardo GiraldoMontero <carloseduardogiraldomontero@gmail.com>
2022-08-11 19:21:02 -0500

    Add navigation handler to searchBar

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 import Logo from '../../assets/Logo_ML_2x.png';
 import SearchIcon from '../../assets/ic_Search@2x.png';
 import './SearchBar.scss';
 
 export const SearchBar = (): JSX.Element => {
+  const [value, setValue] = React.useState<string>('');
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (search) setValue(search);
+  }, [search]);
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.key === 'Enter') {
+      navigate(`/items?search=${value}`);
+    }
+  };
+
   return (
     <header className="search-bar-container">
       <img src={Logo} alt="logo" />
-      <input className="search-input" type="search" placeholder="Nunca dejes de buscar" />
-      <button className="search-button" name="search">
+      <input
+        className="search-input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        onKeyPress={handleKeyPress}
+        type="search"
+        name="search"
+        aria-label="search an item"
+        placeholder="Nunca dejes de buscar"
+      />
+      <button
+        className="search-button"
+        onClick={() => navigate(`/items?search=${value}`)}
+        name="search"
+      >
         <img className="search-icon" src={SearchIcon} alt="search-icon" />
       </button>
     </header>
